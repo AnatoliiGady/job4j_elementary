8e50a656fc5da795301e523417a963fd7590bc34
Гадышкины <grek-prost@yandex.ru>
2022-07-30 20:16:35 +0900

    4.2 Результат работы метода

--- src/main/java/ru/job4j/calculator/MathFunc.java
+++ src/main/java/ru/job4j/calculator/MathFunc.java
@@ -1,16 +1,14 @@
 package ru.job4j.calculator;
 
 public class MathFunc {
-    @SuppressWarnings("UnnecessaryLocalVariable")
-    public static int func1(int x) {
-        int y = x * x + 1;
-        return y;
+      public static int func1(int x) {
+          return x * x + 1;
+
     }
 
-    @SuppressWarnings("UnnecessaryLocalVariable")
-    public static int func2(int x) {
-        int y = 1 / x;
-        return y;
+       public static int func2(int x) {
+        return 1 / x;
+
     }
 
     public static void main(String[] args) {
