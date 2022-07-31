c983bad69a7133b1da12bce84b5a93c62ffabde2
Гадышкины <grek-prost@yandex.ru>
2022-07-31 09:48:48 +0900

    6. Идеальный вес

---
+++ src/main/java/ru/job4j/Peremen.java
@@ -0,0 +1,13 @@
+package ru.job4j;
+
+public class Peremen {
+    public static void main(String[] args) {
+        byte age = 32;
+        char gender = 'M';
+        float weight = 1.76F;
+        double bacterium = 1E+6;
+        double space = 7.78E+8;
+        boolean work = true;
+        int move = 100000;
+    }
+}
