9c58ab7b05180ab431e8000690d072198dae9d9c
Гадышкины <grek-prost@yandex.ru>
2022-07-31 09:51:48 +0900

    6. Идеальный вес

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -1,21 +1,19 @@
 package ru.job4j.calculator;
 
 public class Fit {
-    public static double manWeight(short heightMan) {
-        double rsl = (heightMan - 100.0) * 1.15;
-        return rsl;
+    public static double manWeight(short height) {
+        return (height - 100) * 1.15;
     }
 
-    public static double womanWeight(short heightWoman) {
-        double rsl = (heightWoman - 110.0) * 1.15;
-        return rsl;
+    public static double womanWeight(short height) {
+        return (height - 110.0) * 1.15;
     }
 
     public static void main(String[] args) {
-        short heightMan = 176;
-        double man = Fit.manWeight(heightMan);
-        short heightWoman = 170;
-        double woman = Fit.womanWeight(heightWoman);
+        short heigh = 176;
+        double man = Fit.manWeight(heigh);
+        short height = 170;
+        double woman = Fit.womanWeight(height);
         String resultMan = String.format("%.1f", man);
         System.out.println("Man 176 is " + resultMan);
         String resultWoman = String.format("%.1f", woman);
