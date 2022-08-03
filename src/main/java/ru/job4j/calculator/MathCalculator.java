ee168b959302cfdd7831fd8559fa7ea05ce06f90
Гадышкины <grek-prost@yandex.ru>
2022-08-03 19:43:52 +0900

    1.1. Что такое import

--- src/main/java/ru/job4j/calculator/MathCalculator.java
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -3,20 +3,16 @@ import static ru.job4j.math.MathFunction.*;
 
 public class MathCalculator {
     public static double sumAndMultiply(double first, double second) {
-        return ru.job4j.math.MathFunction.sum(first, second)
-                + ru.job4j.math.MathFunction.multiply(first, second);
+        return sum(first, second) + multiply(first, second);
     }
 
     public static double sumMinusAndDiv(double first, double second) {
-        return ru.job4j.math.MathFunction.minus(first, second)
-                + ru.job4j.math.MathFunction.div(first, second);
+        return minus(first, second) + div(first, second);
     }
 
     public static double sumAll(double first, double second) {
-        return ru.job4j.math.MathFunction.sum(first, second)
-                + ru.job4j.math.MathFunction.multiply(first, second)
-                + ru.job4j.math.MathFunction.minus(first, second)
-                + ru.job4j.math.MathFunction.div(first, second);
+        return sum(first, second) + multiply(first, second)
+                + minus(first, second) + div(first, second);
     }
 
     public static void main(String[] args) {
