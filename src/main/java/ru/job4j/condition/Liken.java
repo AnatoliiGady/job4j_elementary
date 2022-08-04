afe099b03358de95ecbaddbb406bdee58dc1f339
Гадышкины <grek-prost@yandex.ru>
2022-08-04 21:31:24 +0900

    0. Операции сравнения

---
+++ src/main/java/ru/job4j/condition/Liken.java
@@ -0,0 +1,17 @@
+package ru.job4j.condition;
+
+public class Liken {
+    public static void main(String[] args) {
+        int first = 8;
+        int second = 9;
+
+        boolean result = first > second;
+        System.out.println(result);
+
+        result = first < second;
+        System.out.println(result);
+
+        result = first == second;
+        System.out.println(result);
+    }
+}
