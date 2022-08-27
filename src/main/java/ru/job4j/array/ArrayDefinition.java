fbfa1f2af00f97f572655f15644ce9dbf7e3fd96
ConferenceRoomNeru <grek-prost@yandex.ru>
2022-08-27 13:49:27 +0900

    6.0.2. Размер массива

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,8 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surname = new String[100500];
         float[] prices = new float[40];
+        System.out.println("размер массива равен: " + ages.length);
+        System.out.println("размер массива равен: " + surname.length);
+        System.out.println("размер массива равен: " + prices.length);
     }
 }
